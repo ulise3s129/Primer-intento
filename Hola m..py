5cffefe5058d529ea01ff4a05f09ee6bfe04038d
ulise3s129 <38160545+ulise3s129@users.noreply.github.com>
2018-04-07 13:34:24 -0500

    primera clase /Primer programa

---
+++ Hola m..py
@@ -0,0 +1,18 @@
+##print("¿Como te llamas?")
+##nombre= input()
+edad= "19" 
+##print("Hola",nombre,"¿como estas?,""\n", "que tal te sientan los",edad,"\n")
+print("numero")
+a = b = input()##L apregunta o el dato pedido se puede poner en los parentesis
+print("Numbre?")
+c=input()
+print("altura?")
+d=int (input())
+print("Sangre?")
+e=input()
+print("ciudad?")
+f= input()
+g= input("libro?")##Este ejemplo complementa el comentario anterior
+h=input("Algun otro dato?")
+print("numero:",a,"\nNombre:",c,"\naltura:",d,"\nSangre:",e,"\nCiudad:", f,"\nLibro favorito:",g,"\nDATO7:",h)
+
